0eb200fbe1e168280f4e54993d1d4abea99ab9da
Sujit8820 <kadamsujit8820@gmail.com>
2022-03-06 11:04:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace User_Registration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to User Registration program");
+        }
+
+    }
+}
