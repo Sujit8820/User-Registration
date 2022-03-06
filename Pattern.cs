333ab2c007ae31bfa82f71ec56671d7a7df14973
Sujit8820 <kadamsujit8820@gmail.com>
2022-03-06 14:00:42 +0530

    [Sujit] ADDED: User need to enter a valid email

--- Pattern.cs
+++ Pattern.cs
@@ -10,6 +10,8 @@ namespace User_Registration
     public class Pattern
     {
         public static string REGEX_NAME = "^[A-Z][a-z]{2,}$";
+        public static string REGEX_EMAIL = "^[a-z]{3,}(.[a-z]{3,})?@bl.co(.in)?$";
+
 
         public void ValidFirstName(string firstName)
         {
@@ -21,6 +23,11 @@ namespace User_Registration
         public void ValidLastName(string lastName)
         {
             string validOrNot = Regex.IsMatch(lastName, REGEX_NAME) ? "Last name is valid" : "Last name is invalid";
+            Console.WriteLine($" {validOrNot}");
+        }
+        public void ValidEmail(string email)
+        {
+            string validOrNot = Regex.IsMatch(email, REGEX_EMAIL) ? "email is valid" : "email is invalid";
             Console.WriteLine($" {validOrNot}");
         }
     }
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace User_Registration
             Console.WriteLine("Wellcome to User Registration program");
             Pattern pattern = new Pattern();
 
-            Console.Write("Enter first name: ");
+           /* Console.Write("Enter first name: ");
             string firstName = Console.ReadLine();
             pattern.ValidFirstName(firstName);
 
@@ -17,6 +17,10 @@ namespace User_Registration
             Console.Write("\nEnter last name: ");
             string lastName = Console.ReadLine();
             pattern.ValidLastName(lastName);
+*/
+            Console.Write("\nEnter email: ");
+            string email = Console.ReadLine();
+            pattern.ValidEmail(email);
 
 
 
