72cd04fdc21c2942e129dd3c5c071b13dba01a41
Sujit8820 <kadamsujit8820@gmail.com>
2022-03-07 15:31:28 +0530

    [ Sujit] ADDED: Password should have at least one upper case

--- Pattern.cs
+++ Pattern.cs
@@ -12,7 +12,7 @@ namespace User_Registration
         public static string REGEX_NAME = "^[A-Z][a-z]{2,}$";
         public static string REGEX_EMAIL = "^[a-z]{3,}(.[a-z]{3,})?@bl.co(.in)?$";
         public static string REGEX_MOBILENO = "^91[ ][6-9]{1}[0-9]{9}$";
-        public static string REGEX_PASSWARD = "^[a-zA-Z0-9]{8,}$";
+        public static string REGEX_PASSWARD = "^(?=.*[A-Z])[1-9a-zA-Z]{1}[a-zA-Z0-9]{7,}$";
 
 
 
